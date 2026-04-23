687e13833485270fcb0a35bda9ab5faf37d3370b
egesacagri <cagriozcanegesa@gmail.com>
2026-04-23 18:10:41 +0300

    Web Bluetooth mimarisi yayına hazır

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // Next.js'e projeyi statik derlemesini söyler
+  basePath: '/dmx-web-remote', // GitHub reponun tam adı (Baştaki slash önemli)
+  assetPrefix: '/dmx-web-remote/', // Sonundaki slash önemli
+  images: {
+    unoptimized: true, // Statik export için resim optimizasyonunu kapatır
+  },
 };
 
 export default nextConfig;
